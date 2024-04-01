6ba311ab5e0100e4645a806dc3f3d66162aec8df
qor8917 <qor8917@gmail.com>
2024-04-01 21:14:27 +0400

    tailwind 기초, 폰트 및 임시 메인 페이지 완료

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-6xl font-roboto-mono">Trueblue-tree 🌳</h1>
+      <h1 className="text-2xl">Comming Soon</h1>
+    </>
+  );
+}
