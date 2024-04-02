5e6b87189cef555ac2c37b711cf192fc8376569f
qor8917 <qor8917@gmail.com>
2024-04-02 08:56:45 +0400

    나무 크기 지정

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 import Image from 'next/image';
+import tree from '../public/trueBlueTree.webp';
 
 export default function Home() {
   return (
     <>
       <div className="flex">
-        <h1 className="text-6xl">Trueblue-tree</h1>
-        <Image
-          src="/trueBlueTree.webp"
-          alt="trueBlueTree"
-          width={60}
-          height={60}
-        />
+        <h1 className="text-2xl">Trueblue-tree</h1>
+        <div className="">
+          <Image src={tree} alt="trueBlueTree" width={32} height={32} />
+        </div>
       </div>
-
-      <h1 className="text-2xl">Comming Soon</h1>
+      <h1 className="text-base">Coming Soon</h1>
     </>
   );
 }
